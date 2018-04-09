aced7ccc2910cffd944b5b1b0af6acbc90a3a3a8
ritupatel <rsp58@njit.edu>
2018-04-08 21:16:43 -0400

    Minor changes

--- app/src/main/java/com/resport/cid/njitresportandroidapplication/FacultyProfile.java
+++ app/src/main/java/com/resport/cid/njitresportandroidapplication/FacultyProfile.java
@@ -308,4 +308,14 @@ public class FacultyProfile extends AppCompatActivity implements NavigationView.
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
--- app/src/main/java/com/resport/cid/njitresportandroidapplication/StudentOpportunityView.java
+++ app/src/main/java/com/resport/cid/njitresportandroidapplication/StudentOpportunityView.java
@@ -232,4 +232,14 @@ public class StudentOpportunityView extends AppCompatActivity
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
